da0120dcf8e06c6a5619e19dd69941d909ecf0ec
Martin Sečkár <Martin.Seckar@ibm.com>
2021-02-21 11:52:22 +0100

    ping + manifest file

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get('/ping', (req, res) => {
+  res.send('pong');
+})
+
 app.listen(port, () => {
   console.log("Server is running on port ", port);
 });
