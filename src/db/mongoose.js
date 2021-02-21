f47ec73daf3a3caf90a0bc7015a1bef3825b32a9
Martin Sečkár <Martin.Seckar@ibm.com>
2021-02-21 15:47:39 +0100

    removed console.log

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 var connectionUrl = process.env.MONGODB_URL;
-console.log(connectionUrl);
 
 mongoose.connect(connectionUrl, {
   useUnifiedTopology: true,
